8d60a42110072de6c56b4f62e957856e1e3d47bd
Viktor Vorobev <viktor.vorobev@emlid.com>
2021-12-17 15:51:25 +0300

    src: components: PostList: show stub if no posts

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div v-if="posts.length >0">
     <h3>Posts from users</h3>
     <post-item
         v-for="post in posts"
@@ -8,6 +8,9 @@
         @remove="$emit('remove', post)"
     />
   </div>
+  <h2 v-else style="color: red">
+    No posts
+  </h2>
 </template>
 
 <script>
