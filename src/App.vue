cf32f8da48ac44460f2218237d3829c8ffe7108f
Viktor Vorobev <viktor.vorobev@emlid.com>
2021-12-17 15:12:53 +0300

    src: components: add remove post functionality

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
     />
     <post-list
         :posts="posts"
+        @remove="removePost"
     />
   </div>
 </template>
@@ -28,9 +29,12 @@ export default {
     }
   },
   methods: {
-    createPost(newPost) {
-      this.posts.push(newPost);
-    }
+    createPost(post) {
+      this.posts.push(post);
+    },
+    removePost(post) {
+      this.posts = this.posts.filter(p => p.id !== post.id)
+    },
   }
 }
 </script>
