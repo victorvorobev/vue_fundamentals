eb3d9b5232201fc7f0d6701cda58ea0df54a23ec
Viktor Vorobev <viktor.vorobev@emlid.com>
2021-12-17 15:56:01 +0300

    src: components: add dialog window component

---
+++ src/components/UI/MyDialog.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="dialog" v-if="show">
+    <div class="dialog__content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "my-dialog",
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.dialog {
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  display: flex;
+}
+
+.dialog__content {
+  margin: auto;
+  background: white;
+  border-radius: 12px;
+  min-height: 50px;
+  min-width: 300px;
+}
+</style>
