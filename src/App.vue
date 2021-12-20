2515112a7ae22841b4fd61de526c941a536f5beb
Viktor Vorobev <viktor.vorobev@emlid.com>
2021-12-20 15:00:34 +0300

    src: app|components: add static navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
+  <navbar></navbar>
   <div class="app">
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {}
+import Navbar from "@/components/UI/Navbar";
+
+export default {
+  components: {
+    Navbar,
+  }
+}
 </script>
 
-<style scoped>
+<style>
 * {
   margin: 0;
   padding: 0;
---
+++ src/components/UI/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="navbar">
+    <div @click="$router.push('/')">VUE3 fundamentals</div>
+    <div class="navbar__btns">
+      <my-button @click="$router.push('/posts')">Posts</my-button>
+      <my-button style="margin-left: 20px" @click="$router.push('/about')">About</my-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.navbar {
+  height: 50px;
+  background-color: lightgrey;
+  box-shadow: 2px 2px 4px gray;
+  display: flex;
+  align-items: center;
+  padding: 0 15px;
+}
+
+.navbar__btns {
+  margin-left: auto;
+}
+</style>
