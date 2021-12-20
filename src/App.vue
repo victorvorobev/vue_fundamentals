568ad1fa17e5fd9556caaba6872228fd3da6244e
Viktor Vorobev <viktor.vorobev@emlid.com>
2021-12-20 13:39:37 +0300

    src: app: add fetching by page number

--- src/App.vue
+++ src/App.vue
@@ -27,6 +27,17 @@
         v-if="!isPostsLoading"
     />
     <div v-else>Loading...</div>
+    <div class="page__wrapper">
+      <div
+          v-for="pageNumber in totalPages"
+          :key="pageNumber"
+          class="page"
+          :class="{'current-page': page === pageNumber}"
+          @click="changePage(pageNumber)"
+      >
+        {{ pageNumber }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -49,6 +60,9 @@ export default {
       isPostsLoading: false,
       selectedSort: '',
       searchQuery: '',
+      page: 1,
+      limit: 10,
+      totalPages: 0,
       sortOptions: [
         {value: 'title', name: 'by title'},
         {value: 'body', name: 'by body'},
@@ -66,10 +80,19 @@ export default {
     showDialog() {
       this.dialogVisible = true;
     },
+    changePage(pageNumber) {
+      this.page = pageNumber;
+    },
     async fetchPosts() {
       try {
         this.isPostsLoading = true;
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+          params: {
+            _page: this.page,
+            _limit: this.limit
+          }
+        });
+        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit);
         this.posts = response.data;
       } catch (e) {
         alert("Something went wrong: \n" + e);
@@ -90,6 +113,11 @@ export default {
     sortedAndSearchedPosts() {
       return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
     },
+  },
+  watch: {
+    page() {
+      this.fetchPosts();
+    }
   }
 }
 </script>
@@ -109,6 +137,20 @@ export default {
   display: flex;
   justify-content: space-between;
   margin: 15px 0;
+}
+
+.page__wrapper {
+  display: flex;
+  margin-top: 15px;
+}
+
+.page {
+  border: 1px solid black;
+  padding: 10px
+}
+
+.current-page {
+  border: 2px solid teal;
 }
 
 </style>
