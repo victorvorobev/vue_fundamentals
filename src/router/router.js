8908571e3c8a11e7c38dcfc63adb4353a8569593
Viktor Vorobev <viktor.vorobev@emlid.com>
2021-12-20 14:22:41 +0300

    src: router: router: add page with posts

--- src/router/router.js
+++ src/router/router.js
@@ -1,9 +1,11 @@
 import Main from "@/pages/Main";
+import PostPage from "@/pages/PostPage";
 import {createRouter, createWebHistory} from "vue-router";
 
-const routes = [{
-    path: '/', component: Main
-},]
+const routes = [
+    {path: '/', component: Main},
+    {path: '/posts', component: PostPage},
+]
 
 const router = createRouter({
     routes, history: createWebHistory(process.env.BASE_URL)
