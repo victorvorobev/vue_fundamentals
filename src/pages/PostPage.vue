22fd23ced0296eeafab53608820574fa7d0738f2
Viktor Vorobev <viktor.vorobev@emlid.com>
2021-12-20 15:25:50 +0300

    src: directives: create and use intersection directive

--- src/pages/PostPage.vue
+++ src/pages/PostPage.vue
@@ -27,7 +27,7 @@
         v-if="!isPostsLoading"
     />
     <div v-else>Loading...</div>
-    <div ref="observer" class="observer"></div>
+    <div v-intersection="loadMorePosts" class="observer"></div>
   </div>
 </template>
 
@@ -107,17 +107,6 @@ export default {
   },
   mounted() {
     this.fetchPosts();
-    const options = {
-      rootMargin: '0px',
-      threshold: 1.0
-    }
-    const callback = (entries, observer) => {
-      if (entries[0].isIntersecting && this.page < this.totalPages) {
-        this.loadMorePosts()
-      }
-    };
-    const observer = new IntersectionObserver(callback, options);
-    observer.observe(this.$refs.observer);
   },
   computed: {
     sortedPosts() {
