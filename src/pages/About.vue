a4d89ec6a675ed7c417adf70ce10852913c3d87f
Viktor Vorobev <viktor.vorobev@emlid.com>
2021-12-20 14:43:02 +0300

    src: router|pages: add About page

---
+++ src/pages/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>This is a marvelous about page.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,12 @@
 import Main from "@/pages/Main";
 import PostPage from "@/pages/PostPage";
+import About from "@/pages/About";
 import {createRouter, createWebHistory} from "vue-router";
 
 const routes = [
     {path: '/', component: Main},
     {path: '/posts', component: PostPage},
+    {path: '/about', component: About},
 ]
 
 const router = createRouter({
