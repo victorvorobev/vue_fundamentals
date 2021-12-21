e7060f66f76d33c72d6ebec38785d8eb76fe8807
Viktor Vorobev <viktor.vorobev@emlid.com>
2021-12-21 16:32:50 +0300

    src: hooks: setup composition API

--- src/pages/PostPageCompositionApi.vue
+++ src/pages/PostPageCompositionApi.vue
@@ -8,7 +8,6 @@
     />
     <div class="app__btns">
       <my-button
-          @click="showDialog"
       >
         Create post
       </my-button>
@@ -19,25 +18,24 @@
     </div>
     <my-dialog v-model:show="dialogVisible">
       <post-form
-          @create="createPost"
       />
     </my-dialog>
     <post-list
         :posts="sortedAndSearchedPosts"
-        @remove="removePost"
         v-if="!isPostsLoading"
     />
     <div v-else>Loading...</div>
-    <div v-intersection="loadMorePosts" class="observer"></div>
   </div>
 </template>
 
 <script>
 import PostForm from "@/components/PostForm";
 import PostList from "@/components/PostList";
-import axios from 'axios';
 import MySelect from "@/components/UI/MySelect";
 import MyPageWrapper from "@/components/UI/MyPageWrapper";
+import {usePosts} from "@/hooks/usePosts";
+import useSortedPosts from "@/hooks/useSortedPosts";
+import useSortedAndSearchedPosts from "@/hooks/useSortedAndSearchedPosts";
 
 export default {
   components: {
@@ -48,76 +46,26 @@ export default {
   },
   data() {
     return {
-      posts: [],
       dialogVisible: false,
-      isPostsLoading: false,
-      selectedSort: '',
-      searchQuery: '',
-      page: 1,
-      limit: 10,
-      totalPages: 0,
       sortOptions: [
         {value: 'title', name: 'by title'},
         {value: 'body', name: 'by body'},
       ]
     }
   },
-  methods: {
-    createPost(post) {
-      this.posts.push(post);
-      this.dialogVisible = false;
-    },
-    removePost(post) {
-      this.posts = this.posts.filter(p => p.id !== post.id)
-    },
-    showDialog() {
-      this.dialogVisible = true;
-    },
-    async fetchPosts() {
-      try {
-        this.isPostsLoading = true;
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
-          params: {
-            _page: this.page,
-            _limit: this.limit
-          }
-        });
-        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit);
-        this.posts = response.data;
-      } catch (e) {
-        alert("Something went wrong: \n" + e);
-      } finally {
-        this.isPostsLoading = false;
-      }
-    },
-    async loadMorePosts() {
-      try {
-        this.page += 1;
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
-          params: {
-            _page: this.page,
-            _limit: this.limit
-          }
-        });
-        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit);
-        this.posts = [...this.posts, ...response.data];
-      } catch (e) {
-        alert("Something went wrong: \n" + e);
-      }
-    },
-  },
-  mounted() {
-    this.fetchPosts();
-  },
-  computed: {
-    sortedPosts() {
-      return [...this.posts].sort((post1, post2) => {
-        return post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
-      })
-    },
-    sortedAndSearchedPosts() {
-      return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
-    },
+  setup(props) {
+    const {posts, totalPages, isPostsLoading} = usePosts(10);
+    const {sortedPosts, selectedSort} = useSortedPosts(posts);
+    const {searchQuery, sortedAndSearchedPosts} = useSortedAndSearchedPosts(sortedPosts)
+    return {
+      posts,
+      totalPages,
+      isPostsLoading,
+      sortedPosts,
+      selectedSort,
+      searchQuery,
+      sortedAndSearchedPosts,
+    }
   }
 }
 </script>
